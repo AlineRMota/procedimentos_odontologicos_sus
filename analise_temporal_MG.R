7faa345aecadd150c79e6a5e70da23f07a3b2ed7
Aline Rodrigues Mota <149389028+AlineRMota@users.noreply.github.com>
2026-02-03 13:04:17 -0300

    Add files via upload
    # Procedimentos Odontológicos em Minas Gerais
    
    Este projeto é uma **simulação de análise de procedimentos odontológicos** usando RStudio, com dados fictícios, em Minas Gerais.  
    
    ## Script
    - 'analise_temporal_MG.R'
    
    ## Observação
    Os dados são **fictícios**, pois o site do TabNet/DATASUS estava instável no momento da criação do projeto.

---
+++ analise_temporal_MG.R
@@ -0,0 +1,8 @@
+# =========================================================
+# Projeto: Procedimentos odontológicos no SUS
+# Script: 01_importacao_dados.R
+# Objetivo: Importar dados do TabNet (SIA/SUS)
+# Autora: Aline R. Mota
+# Período: 2023 - 2025
+# Data: 2026
+# =========================================================
